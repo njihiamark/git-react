cfc145ecb6373d66e9a83ff2a667f0e3566c76cd
njihiamark <njihiamark@gmail.com>
2021-12-30 05:18:57 +0300

    clear cache

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Home from "../src/pages/Home";
+
+function App() {
+  
+  return (
+    <div className="bg-gray-100 inline-block w-full">
+      <div className="min-h-screen">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
